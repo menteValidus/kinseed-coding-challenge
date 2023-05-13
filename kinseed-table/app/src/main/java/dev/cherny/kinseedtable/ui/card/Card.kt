992c0260e36a953e600e100ef978600b5f8d70b9
Denis Cherny <39064926+menteValidus@users.noreply.github.com>
2023-05-13 16:30:47 +0400

    Create Card.kt

---
+++ kinseed-table/app/src/main/java/dev/cherny/kinseedtable/ui/card/Card.kt
@@ -0,0 +1,67 @@
+package dev.cherny.kinseedtable.ui.card
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.cherny.kinseedtable.ui.theme.KinseedTableTheme
+
+data class Card(
+    val id: String,
+    val firstName: String,
+    val lastName: String,
+    val createdDate: String,
+    val company: String,
+    val status: String,
+    val fixedLinePhone: String,
+    val mobilePhone: String,
+    val email: String
+)
+
+@Composable
+fun Card(modifier: Modifier = Modifier, card: Card) {
+    Column(modifier = modifier) {
+        DetailedField(details = "ID", value = card.id)
+        DetailedField(details = "Name", value = "${card.firstName} ${card.lastName}")
+        DetailedField(details = "Created at", value = card.createdDate)
+        DetailedField(details = "Company", value = card.company)
+        DetailedField(details = "Status", value = card.status)
+        DetailedField(details = "Fixed-line phone", value = card.fixedLinePhone)
+        DetailedField(details = "Mobile phone", value = card.mobilePhone)
+        DetailedField(details = "E-mail", value = card.email)
+    }
+}
+
+@Composable
+private fun DetailedField(
+    modifier: Modifier = Modifier,
+    details: String,
+    value: String
+) {
+    Row(modifier = modifier) {
+        Text(modifier = Modifier.padding(end = 8.dp), text = "$details:")
+        Text(text = value)
+    }
+}
+
+@Preview
+@Composable
+private fun Card_Preview() {
+    KinseedTableTheme {
+        Card(card = Card(
+            id = "id",
+            firstName = "firstName",
+            lastName = "lastName",
+            createdDate = "createdDate",
+            company = "company",
+            status = "status",
+            fixedLinePhone = "fixedLinePhone",
+            mobilePhone = "mobilePhone",
+            email = "email",
+        ))
+    }
+}
